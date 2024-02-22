5ce7006f7d946b0fc359fee3b8cf6240c5ce80bb
pravind002 <parvindraj004@gmail.com>
2024-02-22 22:43:23 +0530

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
-// Create connection to MySQL database
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -9,16 +9,6 @@ const db = mysql.createConnection({
   database: "learn_db",
 });
 
-//*******CROACOACH DATABASE CONNECTION*************///
-// const db = mysql.createConnection({
-//   host: 'locadusty-civet-8343.8nk.gcp-asia-southeast1.cockroachlabs.cloudlhost',
-//   user: 'parvind_raj',
-//   password: '',
-//   database: 'demo_database',
-//   port:26257
-// });
-
-// Connect
 db.connect((err) => {
   if (err) {
     throw err;
@@ -28,43 +18,28 @@ db.connect((err) => {
 
 const app = express();
 
-// Create table if not exists
-// const createTableQuery = `
-//   CREATE TABLE IF NOT EXISTS users (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-//     name VARCHAR(255),
-//     email VARCHAR(255)
-//   )
-// `;
+// CORS configuration for all origins
+app.use(cors());
 
-// db.query(createTableQuery, (err, result) => {
-//   if (err) throw err;
-//   console.log('Users table created or already exists');
-// });
-
-// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Create user
 app.post("/users", (req, res) => {
   const { name, email, password } = req.body;
-  const insertQuery = `INSERT INTO users (name, email,password) VALUES (?, ?,?)`;
+  const insertQuery = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
   db.query(insertQuery, [name, email, password], (err, result) => {
     if (err) throw err;
     res.send("User added successfully");
   });
 });
 
-// Get all users
 app.get("/users", (req, res) => {
   const selectQuery = `SELECT * FROM users WHERE status = '1'`;
   db.query(selectQuery, (err, results) => {
     if (err) throw err;
-    res.json({"status":"Success","message":"Data get successfully","data":results});
+    res.json({ "status": "Success", "message": "Data get successfully", "data": results });
   });
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
